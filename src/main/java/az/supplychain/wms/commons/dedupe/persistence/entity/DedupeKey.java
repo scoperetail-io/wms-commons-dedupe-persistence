6fe6aae0825df3a474441df134e2bed2d7c8c234
Mohammad Yasir <mohammed.yasir@autozone.com>
2024-01-31 05:23:00 -0600

    feature/wmsrebld-1735: Resolving wms-template-service runtime issue. crt_ts column not found

--- src/main/java/az/supplychain/wms/commons/dedupe/persistence/entity/DedupeKey.java
+++ src/main/java/az/supplychain/wms/commons/dedupe/persistence/entity/DedupeKey.java
@@ -6,7 +6,7 @@
  */
 package az.supplychain.wms.commons.dedupe.persistence.entity;
 
-import az.supplychain.wms.commons.shared.kernel.domain.BaseMetadataEntity;
+import az.supplychain.wms.commons.shared.kernel.domain.BaseCreateTsEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
@@ -21,7 +21,7 @@ import lombok.EqualsAndHashCode;
 @Table(name = "dedupe_key")
 @Data
 @EqualsAndHashCode(of = {"logKey"}, callSuper = false)
-public class DedupeKey extends BaseMetadataEntity {
+public class DedupeKey extends BaseCreateTsEntity {
 
     @Id
     @Column(name = "log_key")
