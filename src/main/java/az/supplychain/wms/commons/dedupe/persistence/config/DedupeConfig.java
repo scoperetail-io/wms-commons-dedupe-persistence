5b14dade9e902bfe18086fac84f4c981d1acef32
Vinay Kumar Reddy Munagala <vinay.munagala@autozone.com>
2024-09-11 05:25:50 +0000

    chore/wmsrebld-10212: initial release versions for parent pom and bom internal/external

---
+++ src/main/java/az/supplychain/wms/commons/dedupe/persistence/config/DedupeConfig.java
@@ -0,0 +1,17 @@
+/*
+ *  DedupeConfig.java
+ *  Copyright 2024 AutoZone, Inc.
+ *  Content is confidential to and proprietary information of AutoZone, Inc.,
+ *  its subsidiaries and affiliates.
+ */
+package az.supplychain.wms.commons.dedupe.persistence.config;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+/** The Configuration class for dedupe persistence component */
+@Configuration
+@EnableJpaRepositories(basePackages = "az.supplychain.wms.commons.dedupe.persistence")
+@EntityScan(basePackages = "az.supplychain.wms.commons.dedupe.persistence")
+public class DedupeConfig {}
